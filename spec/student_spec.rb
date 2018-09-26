c03706bce0f5acffe8f966077c643c7ba62814ae
Michael Roose <mjroose@gmail.com>
2018-09-26 00:00:40 +0000

    automatically backed up by learn

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -78,7 +78,7 @@ describe Student do
         pat.name = "Pat"
         pat.grade = 12
         pat.save
-
+        binding.pry
         pat_from_db = Student.find_by_name("Pat")
         expect(pat_from_db.name).to eq("Pat")
         expect(pat_from_db).to be_an_instance_of(Student)
